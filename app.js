8fa7c3d8663366163d79758b49b0c7d1d07d8532
Kade <kaderderk@gmail.com>
2019-12-26 16:54:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!, This JENKINS PIPELINE WITH DOCKER'))
+app.get('/infos',(reg,res)=> res.send("This is Kade Derk"))
 
 app.set('port', process.env.PORT || 9090)
 app.listen(app.get('port'), () => console.log('Example app listening on port ' + app.get('port') + '!'))
