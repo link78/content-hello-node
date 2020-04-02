87710d9ed6bf02f290d0b36184c524e6a260ce3c
Kade <kaderderk@gmail.com>
2020-04-01 19:54:22 -0500

    Update app.js
    added my blog

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!, This JENKINS PIPELINE WITH Kubernetes and Docker'))
 app.get('/infos',(reg,res)=> res.send("This is Kade Derk, deploying nodejs app on k8s and jenkins pipeline"))
+app.get('/blog',(reg,res)=> res.send("This is my blog fasocloud.blogspot.com/"))
 
 app.set('port', process.env.PORT || 9090)
 app.listen(app.get('port'), () => console.log('Example app listening on port ' + app.get('port') + '!'))
